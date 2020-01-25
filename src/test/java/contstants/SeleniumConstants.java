f7ac4349923b28c2f6daff3b25748db2eac36dec
unknown <darchievboris@gmail.com>
2020-01-24 20:55:21 -0500

    added all the utility classes

---
+++ src/test/java/contstants/SeleniumConstants.java
@@ -0,0 +1,8 @@
+package contstants;
+
+public class SeleniumConstants {
+    public static final long IMPLICIT_WAIT = 10;
+    public static final long EXPLICIT_WAIT = 20;
+    public static final long PAGE_LOAD_TIME = 20;
+
+}
